2720953f0b777cd24374b29470687401db5bc9a4
Timb42 <133685540+Timb42@users.noreply.github.com>
2023-05-16 15:02:14 -0300

    Lon/Lat

--- GeoLocationAPI/script.js
+++ GeoLocationAPI/script.js
@@ -1,6 +1,9 @@
 navigator.geolocation.getCurrentPosition(
     function (position) {
-        console.log(position);
+       // console.log(position);
+       const latitude = position.coords.latitude;
+       const longitude = position.coords.longitude;
+       console.log(latitude, longitude);
     },
     function () {
         alert('Could not get position.');
